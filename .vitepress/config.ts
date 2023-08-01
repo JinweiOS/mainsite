c1a468eb49adc5f8b144c64d16e876f1cd75470a
JinweiOS <jinwei.peng@beingthink.com>
2023-08-01 15:35:13 +0800

    feat: remote default theme VPNavBar.has-sidebar.curtain::before css style

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vitepress'
+import { fileURLToPath, URL } from 'node:url'
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
+  vite: {
+    resolve: {
+      alias: [
+        {
+          find: /^.*\/VPNavBar\.vue$/,
+          replacement: fileURLToPath(
+            new URL('./theme/src/CustomNavBar.vue', import.meta.url)
+          )
+        }
+      ]
+    }
+  },
   title: "BeingThink",
   description: "Make some useful and easy-to-use tools.",
   themeConfig: {
---
+++ .vitepress/theme/index.ts
@@ -0,0 +1,7 @@
+import Layout from './src/layout.vue'
+import DefaultTheme from 'vitepress/theme-without-fonts'
+import './beingthink.css'
+
+export default {
+  extends: DefaultTheme,
+}
