feffd099fea4c4b0cf87cac71e387bb007da87ff
JinweiOS <jinwei.peng@beingthink.com>
2023-10-11 16:46:00 +0800

    feat: rainbow css

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import BeingThink from './src/layout.vue'
 import {h} from 'vue'
 import DefaultTheme from 'vitepress/theme'
 import './color.css'
+import './rainbow.css'
 import './over.css'
 
 export default {
